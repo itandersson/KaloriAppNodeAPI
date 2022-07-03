2e3e60509cee363e81d26bd47155dcae3d9b1288
Joakim Andersson <joakim@ktv-sjobo.se>
2022-07-03 15:50:45 +0200

    Updating route - Api can now send out info about sugar and cocoa

--- routes/calculate_calorie_content.js
+++ routes/calculate_calorie_content.js
@@ -1,10 +1,25 @@
 'use strict';
+//If CONNECTIONSTRING is not assigned
+if (process.env.CONNECTIONSTRING == null) { process.env.CONNECTIONSTRING = 'mongodb://root:password@localhost:27017/livsmedel?authSource=admin'; }
+
 var express = require('express');
 var router = express.Router();
+const Livsmedel = require("../models/livsmedelModel");
 
-/* GET food listing. */
-router.get('/', function (req, res) {
-    res.send("253");
+/* GET param. */
+router.get('/:param', function (req, res) {
+    const param = req.params.param;
+
+    /* Search for the document */
+    Livsmedel.findOne({ vara: param }, function (err, livsmedel) {
+        if (err) {
+            console.log(err);
+        } else if (livsmedel === null) {
+            res.json({ api: 'Finns inte i databasen!' });
+        } else {
+            res.json({ api: livsmedel });
+        }
+    });
 });
 
 module.exports = router;
