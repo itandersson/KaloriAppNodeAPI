d1c414a4795a3550b39d0dacde8f48773f470724
Joakim Andersson <joakim@ktv-sjobo.se>
2022-07-05 13:09:22 +0200

    Deletes commands

--- app.js
+++ app.js
@@ -2,33 +2,19 @@
 var debug = require('debug')('my express app');
 var express = require('express');
 var path = require('path');
-//var favicon = require('serve-favicon');
-//var logger = require('morgan');
-//var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-//var users = require('./routes/users');
 var calculate_calorie_content = require('./routes/calculate_calorie_content');
 
 var app = express();
 
-// view engine setup
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'pug');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-//app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-//app.use('/users', users);
 app.use('/calculate_calorie_content', calculate_calorie_content);
-
 
 
 app.set('port', process.env.PORT || 3000);
